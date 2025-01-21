ee2c115d5ecc004432d56bf5aade71e192cd5cd1
Garcia-IGC <ignacio.garcia02@alumnos.ucn.cl>
2025-01-21 17:52:41 +0000

    confilmo confilmo

--- main.cpp
+++ main.cpp
@@ -5,6 +5,24 @@
 
 using namespace std;
 
+class Nodo(){
+
+    pair<char,int> ci;
+    vector<Nodo*> hijos;
+
+};
+
+vector<vector<int>> armarMatriz(string nombreTXT){
+
+
+
+
+}
+
+void dijkstra(vector<int>& menorCamino, vector<vector<int>>& matAdy,Nodo* raiz){
+    return;
+}
+
 int main(){
 
     cout<<"Hello World"<<endl;
